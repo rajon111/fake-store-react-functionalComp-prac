5b591c392c402b430659c1a1d4b8d13bce0f312d
rajon <mostofarajon111@gmail.com>
2022-03-20 14:20:46 +0600

    aos animation added

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactModal from '../ReactModal/ReactModal';
 import './SingleProduct.css'
 
+
+
 const SingleProduct =(props) => {
    
     const{title, image}=props.product
     const {cartIncrease} =props
     console.log(props)
     return (
-        <div className='col-md-4'>
+        <div data-aos="zoom-out-left"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="300"
+        data-aos-offset="0" className='col-md-4'>
             <div className="card p-2 border">
             <h1>Title:{title.slice(0,10)}</h1>
              <img className='w-50 m-auto' src={image} alt="" />   
